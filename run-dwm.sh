2a4b8141dcbb491806f7a5d542b787552f9ba14a
Sacules <leociancalucas@gmail.com>
2018-06-29 21:51:03 -0300

    nicer dwmstatus

--- run-dwm.sh
+++ run-dwm.sh
@@ -4,17 +4,17 @@ xrdb -merge -I$HOME ~/.Xresources
 
 # my stuff
 while true; do
-BATT=$( acpi -b | sed 's/.*[charging|unknown], \([0-9]*\)%.*/\1/gi' )
-STATUS=$( acpi -b | sed 's/.*: \([a-zA-Z]*\),.*/\1/gi' )
-DATE=$(date +"%I:%M | %d %m %y")
+    BATT=$( acpi -b | sed 's/.*[charging|unknown], \([0-9]*\)%.*/\1/gi' )
+    STATUS=$( acpi -b | sed 's/.*: \([a-zA-Z]*\),.*/\1/gi' )
+    DATE=$(date +"%a %d %b, %R")
 
-xsetroot -name "`echo $DATE \| $BATT\% $STATUS`" # status bar
-sleep 30
+    xsetroot -name "`echo $BATT\% $STATUS \| $DATE `" # status bar
+    sleep 30
 done &
 
 xset fp+ /usr/share/fonts/ &			 # font stuff
 xset fp rehash &
-compton --config ~/compton.conf &
+compton --config compton.conf &
 hsetroot -fill ~/Pictures/Wallpaper/5000237-metroid-prime-wallpapers.jpg 			 # cool af wallpaper
 xrandr --output LVDS-1 --brightness 0.8
 setxkbmap -layout latam -variant deadtilde
