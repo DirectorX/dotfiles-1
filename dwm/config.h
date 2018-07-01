caa90cc2379fb94102d45f2fc61ebf90c4a5d5e0
sacules <leociancalucas@gmail.com>
2018-07-01 00:28:13 -0300

    smaller borders

--- dwm/config.h
+++ dwm/config.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 8;        /* gap pixel between windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
